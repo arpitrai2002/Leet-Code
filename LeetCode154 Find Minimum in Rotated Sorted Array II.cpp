c9fed979d809c7df1cd6806332512d9fef0c64ae
Arpit Rai <arpitr2002@outlook.com>
2024-01-28 03:14:59 +0530

    LeetCode154

---
+++ LeetCode154 Find Minimum in Rotated Sorted Array II.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int Pivot(vector<int> arr){
+        int low=0;
+        int high=arr.size()-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(low==high){
+                return mid;
+            }
+            if(mid+1<=high && arr[mid]>=arr[mid+1]){
+                return mid;
+            }
+            if(mid-1>=low && arr[mid-1]>=arr[mid]){
+                return mid-1;
+            }
+            if(arr[low]>arr[mid]){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+    }
+    return -1;
+    }
+    int findMin(vector<int>& nums) {
+        int index=Pivot(nums);
+        int n=nums.size()-1;
+        if(index==-1){
+            return -1;
+        }
+        else{
+            while(index+1<=n && nums[index]==nums[index+1]){
+            index++;
+            continue;
+        }
+            if(index==nums.size()-1){
+                return nums[0];
+            }
+            else{
+                return nums[index+1];
+            }
+        }
+    }
+};
